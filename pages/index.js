49cf363e4a716601057ce271d42e329c7319da46
abilashraghuram <raghuramabilash@gmail.com>
2025-07-26 22:24:28 -0700

    adding form

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,43 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my a!" />
+        <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        
+        {/* Movie Review Form */}
+        <div className="form-container">
+          <h2>Movie Review Form</h2>
+          <form name="movie-review" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="movie-review" />
+            
+            <p>
+              <label>
+                Your Name: 
+                <input type="text" name="name" required />
+              </label>
+            </p>
+            
+            <p>
+              <label>
+                Movie Name: 
+                <input type="text" name="movie-name" required />
+              </label>
+            </p>
+            
+            <p>
+              <label>
+                Movie Review: 
+                <textarea name="movie-review" rows="5" required></textarea>
+              </label>
+            </p>
+            
+            <p>
+              <button type="submit">Submit Review</button>
+            </p>
+          </form>
+        </div>
       </main>
 
       <Footer />
