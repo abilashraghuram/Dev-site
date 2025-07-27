ed98c7c695b8902cd318259146dba9a06f857433
abilashraghuram <raghuramabilash@gmail.com>
2025-07-27 09:15:38 -0700

    movie review

--- pages/index.js
+++ pages/index.js
@@ -42,10 +42,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Review a Movie!!" />
         
         {/* Movie Review Form */}
         <div className="form-container">
